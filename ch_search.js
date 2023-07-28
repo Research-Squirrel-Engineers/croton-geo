f61be3ceebfc4455bb6ba71667a71bdfb92b3850
florianthiery <mail@fthiery.de>
2023-07-28 10:40:04 +0000

    Deploying to gh-pages from @ Research-Squirrel-Engineers/croton-geo@27f75c71aff40c84a0970c83e2868926afbe9488 🚀

---
+++ ch_search.js
@@ -0,0 +1,5 @@
+var search={
+  "Concept Instances Collection": "http://coinhoards.org/id/Concept_collection",
+  "IGCH 1873": "http://coinhoards.org/id/igch1873",
+  "IGCH 1885": "http://coinhoards.org/id/igch1885"
+}
